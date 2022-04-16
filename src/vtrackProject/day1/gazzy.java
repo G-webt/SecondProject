897300d217786e86e08295deb04393050d2d892f
Gazi <guressg@gmail.com>
2022-04-15 22:40:35 -0400

    Before the master, we checked all the codes, and everything is fine. Please check it again.

--- src/vtrackProject/day1/gazzy.java
+++ src/vtrackProject/day1/gazzy.java
@@ -2,6 +2,7 @@ package vtrackProject.day1;
 
 public class gazzy {
 
-    // this is second day,I've been working.
+    // this is second day,I've been working.\
+    // we will pull request into the master right now.
 
 }
